6bf2cfd7185fca687396af3f196951e25639fa28
lauri <lauri.k@adngames.com>
2023-01-24 19:19:39 +0200

    fix eslint errors

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,16 +5,11 @@ import { fileURLToPath } from 'url';
 
 import genDiff from '../index.js';
 
-let __filename;
-let __dirname;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
-
-beforeAll(() => {
-  __filename = fileURLToPath(import.meta.url);
-  __dirname = path.dirname(__filename);
-});
 
 const fileExtensions = ['.json', '.yaml', '.yml'];
 
